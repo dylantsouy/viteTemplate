f6b3493c6a935b83a88607bf1692b7329f468bf1
Dylan Tsou <>
2022-06-30 18:00:27 +0800

    fix base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:"/viteApp/",
+    base:"/viteTemplate/",
     plugins: [
         svgr(),
         react()
