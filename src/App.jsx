f537dc1e759068ed67d33c623ae1b10670c60c40
Dylan Tsou <>
2022-07-01 17:38:30 +0800

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Layout from '@/layouts/main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from '@/containers/dashboard';
+import NoMatch from '@/containers/noMatch';
 
 function App() {
     return (
         <BrowserRouter>
             <Layout>
                 <Routes>
-                    <Route path='/' element={<Dashboard />} />
-                    <Route path='/asd' element={<Dashboard />} />
+                    <Route index element={<Dashboard />} />
+                    <Route path='/viteTemplate/' element={<Dashboard />} />
+                    <Route path='*' element={<NoMatch />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
---
+++ src/containers/noMatch/noMatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <div>NoMatch</div>
+        </div>
+    );
+};
+
+export default NoMatch;
