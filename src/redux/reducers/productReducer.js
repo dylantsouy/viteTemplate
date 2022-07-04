d5f34070434ec2027d87ecd4a32c8611c20d5e84
Dylan <bear817005@gmail.com>
2022-07-05 00:46:24 +0800

    add redux

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from '../constants/actionTypes';
+
+const initialState = {
+    products: [{ id: 1, title: 'Test', category: 'food' }],
+};
+export const productReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+
+        default:
+            break;
+    }
+};
+
